c431d1e71419fdb989e3fa2b726bd1e1b1c47cd2
deeprado <siversonw@126.com>
2020-06-15 17:17:07 +0800

    完善

--- lib/pages/demo/Badger.dart
+++ lib/pages/demo/Badger.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_app_badger/flutter_app_badger.dart';
 
 class BadgerPage extends StatefulWidget {
@@ -9,6 +10,37 @@ class BadgerPage extends StatefulWidget {
 }
 
 class _BadgerPageState extends State<BadgerPage> {
+  String _appBadgeSupported = 'Unknown';
+
+  @override
+  initState() {
+    super.initState();
+    initPlatformState();
+  }
+
+  initPlatformState() async {
+    String appBadgeSupported;
+    try {
+      bool res = await FlutterAppBadger.isAppBadgeSupported();
+      if (res) {
+        appBadgeSupported = 'Supported';
+      } else {
+        appBadgeSupported = 'Not supported';
+      }
+    } on PlatformException {
+      appBadgeSupported = 'Failed to get badge support.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _appBadgeSupported = appBadgeSupported;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,6 +51,9 @@ class _BadgerPageState extends State<BadgerPage> {
         padding: EdgeInsets.all(16.0),
         child: new Column(
           children: <Widget>[
+            new Center(
+              child: Text(_appBadgeSupported),
+            ),
             new MaterialButton(
               color: Colors.blue,
               textColor: Colors.white,
